87bda94c41a210c4612d92b681bd84872d514f5b
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2022-12-07 12:27:51 +0200

    header edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css';
  import Post from './components/post/post.component';
- import ListItem from "./components/List/List.comonents";
+ import ListItem from "./components/List/List.components";
 import PostBook from './components/postBook/PostBook.components';
 import AboutBook from './components/AboutBook/AboutBook.components';
+import Header from './components/header/header.component';
 
 
 function App() {
 
   return (
     <div className="App">
-    
-      <ListItem/>
+      <Header/>
+      {/* <ListItem/>
       <PostBook/>
       <Post/>
-      <AboutBook/>
+      <AboutBook/> */}
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import './header.css';
-// import { Bell, Chats,PlusCircle ,SignIn} from 'phosphor-react';
 // import { Link } from 'react-router-dom';
 
 const Header = () => {
- 
-   
+
+
   return (
     <div className="header">
-      {/* <Link to="/feed"><img src={logo} alt="logo" height="50px" /></Link> */}
-      <div className="actions">
-        
+    <div>
+      <div className="pages_div">
 
-        {/* <Link className="icon-button"><Bell size={24} weight="bold" /></Link>
-        <Link to="/chat" className="icon-button"><Chats size={24} weight="bold" /></Link>
-        <Link to="/addPost" className="icon-button"><PlusCircle size={24} weight="bold" /></Link>
-        <Link to="/login" className="icon-button"><SignIn size={24} weight="bold" /></Link> */}
-
+        <span className="page">
+          Books
+        </span>
+       
+        <span className="page">
+          Audiobooks
+        </span>
+       
+        <span className="page">
+          Podcast
+        </span>
       </div>
+      <hr />
+      </div>
+      <input
+        type="search"
+        name="search"
+        placeholder='Genere, author, or book name  '
+
+      />
     </div>
+
   )
 }
 
