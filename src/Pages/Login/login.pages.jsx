4f0d39904299db118664bccc66fb032375ac53e7
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2022-12-14 17:53:31 +0200

    log in page

--- src/Pages/Login/login.pages.jsx
+++ src/Pages/Login/login.pages.jsx
@@ -1,44 +1,27 @@
 
-import { BookOpen } from 'phosphor-react'
 import './login.css'
+import libr from '../../assets/images/libr.jpg';
 
 const Login = () => {
 
 
   return (
     <div className='login'>
-      <h2>Welcome Back, to our online library</h2>
-
-
+      
       <form action="" class="form">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+
         <div class="form-inner">
-          <h2> <BookOpen size={30} color="#6e8ee8" weight="bold" />    LOGIN </h2>
+          <h2>  Welcome! </h2>
+          <h4>Please, sign in</h4>
+
           <div class="content">
             <input class="input" type="email" placeholder="Email" />
             <input class="input" type="password" placeholder="Password" />
-            <button class="btn">LOGIN</button>
+            <button class="btn">Sign in</button>
           </div>
         </div>
       </form>
-      {/* <h2>Welcome Back, to our online library </h2>
-      <form >
-        <div className='input-group'> html><html lang="en">
-          <label >Email</label>
-          <input type="text" name='email' />
-        </div>
-        <div className='input-group'>
-          <label >Password</label>
-          <input type="password" name='password' />
-        </div>
-        <div className='action-group'>
-          <button type="submit" className='Login'>Login</button>
-        </div>
-      </form> */}
-
+      <img src={libr} alt="" srcset="" />
     </div>
   )
 }
